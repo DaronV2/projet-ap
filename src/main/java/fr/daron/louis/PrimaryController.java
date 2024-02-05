56d1f0f51c6f027020f25853268a2dccbb649905
Louis <darkenciel31@gmail.com>
2024-02-05 16:42:20 +0100

    M

--- src/main/java/fr/daron/louis/PrimaryController.java
+++ src/main/java/fr/daron/louis/PrimaryController.java
@@ -2,13 +2,25 @@ package fr.daron.louis;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
+
+    @FXML
+    private TextField login;
+
+    @FXML
+    private PasswordField passw;
+
+    @FXML
+    private Button primaryButton;
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
 
-    
+
 }
