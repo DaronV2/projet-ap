6cff964e82ee7806afa4276404f0c018d36a1ebd
Leo-Wls <leowalers@icloud.com>
2024-02-19 15:51:18 +0100

    AjoutBtnAccueil

--- src/main/java/fr/daron/louis/SecondaryController.java
+++ src/main/java/fr/daron/louis/SecondaryController.java
@@ -1,12 +1,65 @@
 package fr.daron.louis;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class SecondaryController {
 
     @FXML
-    private void switchToPrimary() throws IOException {
+    private Button accueilBtn;
+
+    @FXML
+    private TextField afDate1;
+
+    @FXML
+    private TextField afDate2;
+
+    @FXML
+    private TextField afLibelle1;
+
+    @FXML
+    private TextField afLibelle2;
+
+    @FXML
+    private TextField afMontant1;
+
+    @FXML
+    private TextField afMontant2;
+
+    @FXML
+    private TextField km;
+
+    @FXML
+    private TextField matricule;
+
+    @FXML
+    private TextField mois;
+
+    @FXML
+    private TextField montantUnitaireKm;
+
+    @FXML
+    private TextField nom;
+
+    @FXML
+    private TextField nuitee;
+
+    @FXML
+    private TextField repasMidi;
+
+    @FXML
+    private TextField totalNuitee;
+
+    @FXML
+    private TextField totalRepasMidi;
+
+    @FXML
+    void switchAccueil(ActionEvent event) throws IOException{
         App.setRoot("primary");
     }
+
 }
