dfc4949ecff169e1378454133b4a54e64f2262ae
Louis <darkenciel31@gmail.com>
2024-03-15 13:15:16 +0100

    commit bug

--- src/main/java/fr/daron/louis/ThirdController.java
+++ src/main/java/fr/daron/louis/ThirdController.java
@@ -1,14 +1,37 @@
 package fr.daron.louis;
 
+import java.io.File;
 import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.application.HostServices;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
-public class ThirdController {
+public class ThirdController extends Application{
+
+    @FXML
+    private ImageView doc1;
+
     @FXML
     void accueil(ActionEvent event) throws IOException {
         App.setRoot("primary");
     }
 
+    @FXML
+    void pdf(MouseEvent event) throws IOException {
+        System.out.println("marche");
+        File file = new File("C:\\Users\\darke\\OneDrive\\Documents\\cv.pdf");
+        HostServices hostServices = getHostServices();
+        hostServices.showDocument(file.getAbsolutePath());
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
 }
